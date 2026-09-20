75c82fcf6c7e73a6e0c4a8f12ecafa58423e2051
RemonHassan <fmremonhassan203@gmail.com>
2026-09-21 01:09:33 +0600

    env added

--- src/components/homepage/Books.tsx
+++ src/components/homepage/Books.tsx
@@ -1,10 +1,17 @@
 import { IBook } from "@/types/books.type";
 import BookCard from "../shared/BookCard";
+import { error } from "console";
 
 const getBooks = async () => {
-  const res = await fetch("http://localhost:3000/booksData.json");
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
 };
 const Books = async () => {
   const booksData = await getBooks();
